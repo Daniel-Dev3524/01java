d76737f67a1e0c3949feaa2f1f75f728bb8ba48f
Daniel da Silva <daniel-lg@hotmail.com>
2023-07-31 21:59:31 -0300

    primeira aula

---
+++ Atividade01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Atividade01 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Celsius:");
+        Float tempCelsius = scanner.nextFloat();
+
+
+    }
+}
